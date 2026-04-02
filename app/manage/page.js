b603e83440fc792eb3d2960e8ebfb26ac83914a5
Verity <verity.boyd@edu.sait.ca>
2026-04-02 17:23:29 -0600

    added site header to search and manage pages

--- app/manage/page.js
+++ app/manage/page.js
@@ -1,1 +1,14 @@
 //manage archive page/will link to the add new production page
+
+import SiteHeader from "../components/SiteHeader";
+
+export default function Page() {
+  return (
+    <main>
+      <header>
+        <SiteHeader />
+      </header>
+      Jenna page
+    </main>
+  );
+}
--- app/search/page.js
+++ app/search/page.js
@@ -1,1 +1,14 @@
-//search results page? not even needed maybe because the "search" page is just the home (page.js) unless that page.js should be the login bc everything else is protected
+//might need to add another page.js for results specifically, page would push to "/results" or something like that (dynamic routing? probs don't need to worry about that atp)
+
+import SiteHeader from "../components/SiteHeader";
+
+export default function Page() {
+  return (
+    <main>
+      <header>
+        <SiteHeader />
+      </header>
+      Aurora page
+    </main>
+  );
+}
